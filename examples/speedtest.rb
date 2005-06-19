714ab7b91c1ebfe7054639842b014ebaafb7ada5
Brian Candler <B.Candler@pobox.com>
2005-06-19 12:28:10 +0000

    *** empty log message ***

--- examples/speedtest.rb
+++ examples/speedtest.rb
@@ -3,19 +3,23 @@
 require 'ldap'
 
 CHILDREN = 10
-CHILD_OPS = 100
+CONNECTS = 1	# per child
+SEARCHES = 100	# per connection
 
 pids = []
 CHILDREN.times do
   pids << fork do
-    conn = LDAP::Conn.new("localhost",1389)
-    conn.set_option(LDAP::LDAP_OPT_PROTOCOL_VERSION, 3)
-    conn.bind
-    CHILD_OPS.times do
-      res = conn.search("uid=1,dc=example,dc=com", LDAP::LDAP_SCOPE_BASE,
-                        "(objectclass=*)") do |e|
-        #puts "#{$$} #{e.dn.inspect}"
+    CONNECTS.times do
+      conn = LDAP::Conn.new("localhost",1389)
+      conn.set_option(LDAP::LDAP_OPT_PROTOCOL_VERSION, 3)
+      conn.bind
+      SEARCHES.times do
+        res = conn.search("uid=1,dc=example,dc=com", LDAP::LDAP_SCOPE_BASE,
+                          "(objectclass=*)") do |e|
+          #puts "#{$$} #{e.dn.inspect}"
+        end
       end
+      conn.unbind
     end
   end
 end
