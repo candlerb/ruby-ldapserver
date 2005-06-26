9de5e895e82e106fce23729070d9689986e14f6e
Brian Candler <B.Candler@pobox.com>
2005-06-26 23:08:17 +0000

    Schema work in progress

--- lib/ldap/server/schema.rb
+++ lib/ldap/server/schema.rb
@@ -14,52 +14,73 @@ class Server
       end
     end
 
-    class AttributeType
-      NUMERICOID = '(\d[\d.]+\d)'
-      QDESCR = '\s* \' [a-zA-Z][a-zA-Z0-9;-]* \' \s*'
-      QDESCRS = "( #{QDESCR} | \\s* \\( (#{QDESCR})+ \\) \\s* )"
-      QDSTRING = '\s* \' (.*?) \' \s*'
-      WOID = '\s* ( [a-zA-Z][a-zA-Z0-9;-]* | \d[\d.]+\d ) \s*'
-      NOIDLEN = '(\d[\d.]+\d) (\{\d+\})?'
-      ATTRIBUTEUSAGE = '(userApplications|directoryOperation|distributedOperation|dSAOperation)'
+    # Shared constants for the regexp-based syntax parsers
 
+    KEYSTR = "[a-zA-Z][a-zA-Z0-9;-]*"
+    NUMERICOID = "( \\d[\\d.]+\\d )"
+    WOID = "\\s* ( #{KEYSTR} | \\d[\\d.]+\\d ) \\s*"
+    _WOID = "\\s* (?: #{KEYSTR} | \\d[\\d.]+\\d ) \\s*"
+    OIDS = "( #{_WOID} | \\s* \\( #{_WOID} (?: \\$ #{_WOID} )* \\) \\s* )"
+    _QDESCR = "\\s* ' #{KEYSTR} ' \\s*"
+    QDESCRS = "( #{_QDESCR} | \\s* \\( (?:#{_QDESCR})+ \\) \\s* )"
+    QDSTRING = "\\s* ' (.*?) ' \\s*"
+    NOIDLEN = "(\\d[\\d.]+\\d) (?: \\{ (\\d+) \\} )?"
+    ATTRIBUTEUSAGE = "(userApplications|directoryOperation|distributedOperation|dSAOperation)"
+
+    SCAN_WOID = /#{WOID}/x
+
+    # shared functions for converting "( oid $ oid ... )" to array and
+    # back again
+
+    def self.oidlist_to_a(str)
+      ans = []
+    end
+
+    #####################################################################
+
+    # Class holding an instance of an AttributeTypeDescription (RFC2252 4.2)
+
+    class AttributeType
       PARSER = %r! \A \s* \( \s*
 	#{NUMERICOID} \s*
-	( NAME #{QDESCRS} )?		# capture 2
-        ( DESC #{QDSTRING} )?
-        ( OBSOLETE \s* )?
-        ( SUP #{WOID} )?
-        ( EQUALITY #{WOID} )?
-        ( ORDERING #{WOID} )?
-        ( SUBSTR #{WOID} )?
-        ( SYNTAX \s* #{NOIDLEN} \s* )?	# capture 2
-        ( SINGLE-VALUE \s* )?
-        ( COLLECTIVE \s* )?
-        ( NO-USER-MODIFICATION \s* )?
-        ( USAGE \s* #{ATTRIBUTEUSAGE} )?
+	(?: NAME #{QDESCRS} )?
+        (?: DESC #{QDSTRING} )?
+        (   OBSOLETE \s* )?
+        (?: SUP #{WOID} )?
+        (?: EQUALITY #{WOID} )?
+        (?: ORDERING #{WOID} )?
+        (?: SUBSTR #{WOID} )?
+        (?: SYNTAX \s* #{NOIDLEN} \s* )?	# capture 2
+        (   SINGLE-VALUE \s* )?
+        (   COLLECTIVE \s* )?
+        (   NO-USER-MODIFICATION \s* )?
+        (?: USAGE \s* #{ATTRIBUTEUSAGE} )?
         \s* \) \s* \z !xu
 
-       attr_reader :oid, :names, :desc, :obsolete, :sup, :equality, :ordering
-       attr_reader :substr, :syntax, :maxlen, :singlevalue, :collective
-       attr_reader :nousermod, :usage
+      attr_reader :oid, :names, :desc, :obsolete, :sup, :equality, :ordering
+      attr_reader :substr, :syntax, :maxlen, :singlevalue, :collective
+      attr_reader :nousermod, :usage
 
       def initialize(str)
-        raise "Bad AttributeType #{str.inspect}" unless PARSER =~ str
+        raise "Bad AttributeTypeDescription #{str.inspect}" unless PARSER =~ str
         @oid = $1
-	@desc = $6
-	@obsolete = $7 && true
-	@sup = $9
-	@equality = $11
-	@ordering = $13
-	@substr = $15
-	@syntax = $17
-	@maxlen = $18
-	@singlevalue = $19 && true
-	@collective = $20 && true
-	@nousermod = $21 && true
-	@usage = $23 && $23.intern
+	@desc = $3
+	@obsolete = ! $4.nil?
+	@sup = $5
+	@equality = $6
+	@ordering = $7
+	@substr = $8
+	@syntax = $9
+	@maxlen = $10 && $10.to_i
+	@singlevalue = ! $11.nil?
+	@collective = ! $12.nil?
+	@nousermod = ! $13.nil?
+	@usage = $14 && $14.intern
         # do this last because it replaces last match variables
-        @names = ($3||"").scan(/'(.*?)'/).flatten
+        @names = ($2||"").scan(/'(.*?)'/).flatten
+        # This is the cache of the stringified version. Rather than
+        # initialize to str, we set nil to force it to be rebuilt
+        @def = nil
       end
 
       def name
@@ -70,9 +91,14 @@ class Server
         @oid
       end
 
+      def changed
+        @def = nil
+      end
+
       def to_def
+        return @def if @def
         ans = "( #{@oid} "
-        if not @names or @names.size == 0
+        if @names.nil? or @names.empty?
           # nothing
         elsif @names.size == 1
           ans << "NAME '#{@names[0]}' "
@@ -93,8 +119,91 @@ class Server
         ans << "NO-USER-MODIFICATION " if @nousermod
         ans << "USAGE #{@usage} " if @usage
         ans << ")"
+        @def = ans
       end
     end # class AttributeType
+
+    #####################################################################
+
+    # Class holding an instance of an ObjectClassDescription (RFC2252 4.4)
+
+    class ObjectClass
+      PARSER = %r! \A \s* \( \s*
+	#{NUMERICOID} \s*
+	(?: NAME #{QDESCRS} )?		# capture 2
+        (?: DESC #{QDSTRING} )?
+        (   OBSOLETE \s* )?
+        (?: SUP #{OIDS} )?
+        (?: ( ABSTRACT|STRUCTURAL|AUXILIARY ) \s* )?
+        (?: MUST #{OIDS} )?
+        (?: MAY #{OIDS} )?
+        \s* \) \s* \z !xu
+
+      attr_reader :oid, :names, :desc, :obsolete, :sup, :struct, :must, :may
+
+      def initialize(str)
+        raise "Bad ObjectClassDescription #{str.inspect}" unless PARSER =~ str
+        m = $~
+        @oid = m[1]
+        @names = (m[2]||"").scan(/'(.*?)'/).flatten
+	@desc = m[3]
+	@obsolete = ! m[4].nil?
+	@sup = (m[5]||"").scan(SCAN_WOID).flatten
+        @struct = m[6] ? m[6].downcase.intern : :structural
+        @must = (m[7]||"").scan(SCAN_WOID).flatten
+        @may = (m[8]||"").scan(SCAN_WOID).flatten
+        @def = nil
+      end
+
+      def name
+        @names.first
+      end
+
+      def to_s
+        if @names && @names[0]
+          @names[0]
+        else
+          @oid
+        end
+      end
+
+      def changed
+        @def = nil
+      end
+
+      def to_def
+        return @def if @def
+        ans = "( #{@oid} "
+        if @names.nil? or @names.empty?
+          # nothing
+        elsif @names.size == 1
+          ans << "NAME '#{@names[0]}' "
+        else
+          ans << "NAME ( "
+          @names.each { |n| ans << "'#{n}' " }
+          ans << ") "
+        end
+        ans << "DESC '#{@desc}' " if @desc
+        ans << "OBSOLETE " if @obsolete
+        ans << joinoids("SUP ",@sup," ")
+        ans << "#{@struct.to_s.upcase} " if @struct
+        ans << joinoids("MUST ",@must," ")
+        ans << joinoids("MAY ",@may," ")
+        ans << ")"
+        @def = ans
+      end
+
+      def joinoids(pfx,arr,sfx)
+        return "" unless arr and !arr.empty?
+        return "#{pfx}#{arr}#{sfx}" unless arr.is_a?(Array)
+        a = arr.collect { |elem| elem.to_s }
+        if a.size == 1
+          return "#{pfx}#{a[0]}#{sfx}"
+        else
+          return "#{pfx}( #{a.join(" $ ")} )#{sfx}"
+        end
+      end
+    end # class ObjectClass
 
   end # class Schema
 
--- test/test-schema.rb
+++ test/test-schema.rb
@@ -3,15 +3,21 @@ require 'ldap/server/schema'
 require 'test/unit'
 
 class SchemaTest < Test::Unit::TestCase
+
   def test_parse_attr
 attr = <<ATTR
-( 2.5.4.3 NAME ( 'cn' 'commonName' ) OBSOLETE SINGLE-VALUE COLLECTIVE NO-USER-MODIFICATION USAGE userApplications )
+( 2.5.4.3 NAME 'cn' OBSOLETE EQUALITY 1.2.3 ORDERING 4.5.678 SUBSTR 9.1.1 SYNTAX 4.3.2{58} SINGLE-VALUE COLLECTIVE NO-USER-MODIFICATION USAGE userApplications )
 ATTR
     a = LDAP::Server::Schema::AttributeType.new(attr)
     assert_equal("2.5.4.3", a.oid)
     assert_equal("cn", a.name)
-    assert_equal(["cn", "commonName"], a.names)
+    assert_equal(["cn"], a.names)
     assert(a.obsolete)
+    assert_equal("1.2.3", a.equality)
+    assert_equal("4.5.678", a.ordering)
+    assert_equal("9.1.1", a.substr)
+    assert_equal("4.3.2", a.syntax)
+    assert_equal(58, a.maxlen)
     assert(a.singlevalue)
     assert(a.collective)
     assert(a.nousermod)
@@ -27,10 +33,42 @@ ATTR
     assert_equal(["cn", "commonName"], a.names)
     assert_equal("RFC2256: common name(s) for which the entity is known by", a.desc)
     assert(! a.obsolete)
+    assert_equal("name", a.sup)
     assert(! a.singlevalue)
     assert(! a.collective)
     assert(! a.nousermod)
-    assert_equal("name", a.sup)
     assert_equal(attr.chomp, a.to_def)
+  end
+
+  def test_parse_objectclass
+oc = <<OC
+( 0.9.2342.19200300.100.4.19 NAME 'simpleSecurityObject' DESC 'RFC1274: simple security object' SUP top AUXILIARY MUST userPassword )
+OC
+    a = LDAP::Server::Schema::ObjectClass.new(oc)
+    assert_equal("0.9.2342.19200300.100.4.19", a.oid)
+    assert_equal("simpleSecurityObject", a.name)
+    assert_equal(["simpleSecurityObject"], a.names)
+    assert(! a.obsolete)
+    assert_equal("RFC1274: simple security object", a.desc)
+    assert_equal(["top"], a.sup)
+    assert_equal(:auxiliary, a.struct)
+    assert_equal(["userPassword"], a.must)
+    assert_equal([], a.may)
+    assert_equal(oc.chomp, a.to_def)
+
+oc = <<OC
+( 2.5.6.6 NAME 'person' DESC 'RFC2256: a person' SUP top STRUCTURAL MUST ( sn $ cn ) MAY ( userPassword $ telephoneNumber $ seeAlso $ description ) )
+OC
+    a = LDAP::Server::Schema::ObjectClass.new(oc)
+    assert_equal("2.5.6.6", a.oid)
+    assert_equal("person", a.name)
+    assert_equal(["person"], a.names)
+    assert(! a.obsolete)
+    assert_equal("RFC2256: a person", a.desc)
+    assert_equal(["top"], a.sup)
+    assert_equal(:structural, a.struct)
+    assert_equal(["sn", "cn"], a.must)
+    assert_equal(["userPassword","telephoneNumber","seeAlso","description"], a.may)
+    assert_equal(oc.chomp, a.to_def)
   end
 end
