6074427085868d328887c5c5d582c3b944dd22de
Rafał Rzepecki <divided.mind@gmail.com>
2013-03-22 12:03:53 +0100

    Move from hoe to bundler for gem management. Bump version.

--- lib/ldap/server/version.rb
+++ lib/ldap/server/version.rb
@@ -1,11 +1,5 @@
 module LDAP #:nodoc:
   class Server #:nodoc:
-    class VERSION #:nodoc:
-      MAJOR = 0
-      MINOR = 3
-      TINY  = 1
-    
-      STRING = [MAJOR, MINOR, TINY].join('.')
-    end
+    VERSION = '0.4.0'
   end
 end
